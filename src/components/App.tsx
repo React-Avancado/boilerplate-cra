3552c7733d18bb7c707dbf67e9a159d353c80c7e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-06-26 16:59:35 -0300

    Remove unused stuff and add RA images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -14,8 +11,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
