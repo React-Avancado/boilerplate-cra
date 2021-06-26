fd92c03a67a9801cf482fbe8032420f924b21a72
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-06-26 17:39:04 -0300

    Add Storybook integration

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import GlobalStyles from "../src/styles/global";
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import Main from ".";
+
+export default {
+  title: "Main",
+  component: Main,
+  args: {
+    description: "description default",
+  },
+} as Meta;
+
+export const Basic: Story = (args) => <Main {...args} />;
+Basic.args = {
+  description: "description basic",
+};
+
+export const Default: Story = (args) => <Main {...args} />;
